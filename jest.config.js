86a925f2f8d097be7b4ef3e474cdfc308f98b9f5
Cihan KAYA <cihan.kaya@shipentegra.com>
2025-08-12 10:51:02 +0300

    refactor: remove Jest configuration and related test files

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/nodes/**/*.(test|spec).ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-};
